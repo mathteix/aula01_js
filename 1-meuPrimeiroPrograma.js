73b529ee15892f3a53194965ea37424bc8ffa223
Matheus Teixeira <164585730+mathteix@users.noreply.github.com>
2024-09-06 08:59:54 -0300

    Add files via upload

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,15 @@
+//primeira instrução em JS
+console.log("\nOlá mundo cruel da pogramação\n");
+
+const aluno01="Matheus";
+const aluno02="Julia";
+
+const idadeAluno01 = 16;
+const idadeAluno02 = 16;
+
+const somaIdades = idadeAluno01 + idadeAluno02;
+
+const dupla = aluno01+" e "+aluno02;
+
+console.log("Somos a dupla: "+dupla+"\n");
+console.log("A soma de nossas idades é: "+somaIdades+" anos");
